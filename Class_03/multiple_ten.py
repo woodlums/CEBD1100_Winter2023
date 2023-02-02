3b7164b344c3ef5acb2c5a62b5fb813e54ff0a34
Brendan <brendanwood@live.com>
2023-02-01 20:58:03 -0500

    Changed 10 to ten in multiple checker program.

--- Class_03/multiple_ten.py
+++ Class_03/multiple_ten.py
@@ -5,8 +5,8 @@ num = input("Enter a number to test: ")
 
 if num.isnumeric():
     if int(num) % 10 == 0 and int(num) < 100:
-        print("Yes, the number is less than 100 and a multiple of 10.")
+        print("Yes, the number is less than 100 and a multiple of ten.")
     else:
-        print("No, the number is not less than 100 and a multiple of 10.")
+        print("No, the number is not less than 100 and a multiple of ten.")
 else:
     print("Error, you did not enter a number.")
